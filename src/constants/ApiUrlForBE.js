2207e418cdfa7291b04f62de3fcb4542df1a5819
briteming <briteming@gmail.com>
2025-01-05 21:23:53 -0800

    Update ApiUrlForBE.js

--- src/constants/ApiUrlForBE.js
+++ src/constants/ApiUrlForBE.js
@@ -4,7 +4,7 @@ export default {
    * GitHub Api
    * @method GET
    */
-  githubIssuesApi: 'https://api.github.com/repos/hexh250786313/Blog/issues',
-  githubInfoApi: 'https://proxy.hexh.xyz/proxy/https://api.github.com/repos/briteming/nagb',
+  githubIssuesApi: 'https://api.github.com/repos/briteming/nagb/issues',
+  githubInfoApi: 'https://api.github.com/repos/briteming/nagb',
   githubSearchApi: 'https://api.github.com/search/issues'
 }
